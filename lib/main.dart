aaefacd27cdc31a5e44537936f2706e9d6a6afe0
Banele Mgwevu <earlearlandpearl2@gmail.com>
2023-11-07 13:40:54 +0200

    'more changes'

--- lib/main.dart
+++ lib/main.dart
@@ -6,26 +6,33 @@ import 'dart:math';
 import 'dart:async';
 
 void main() {
-  runApp(ShakeCounterApp());
+  runApp(const ShakeCounterApp());
 }
 
 class ShakeCounterApp extends StatelessWidget {
+  const ShakeCounterApp({Key? key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-       debugShowCheckedModeBanner: false,
-      home: ShakeCounterPage(),
+      debugShowCheckedModeBanner: false,
+      title: 'Shake Counter',
+      home: const ShakeCounterPage(),
     );
   }
 }
 
 class ShakeCounterPage extends StatefulWidget {
+  const ShakeCounterPage({Key? key});
+
   @override
   _ShakeCounterPageState createState() => _ShakeCounterPageState();
 }
 
 class _ShakeCounterPageState extends State<ShakeCounterPage> {
   int shakeCount = 0;
+  int score = 0;
+  int countdown = 5; // Countdown time in seconds
   Random random = Random();
   List<String> diceFaces = [
     'assets/dice1.svg',
@@ -35,9 +42,10 @@ class _ShakeCounterPageState extends State<ShakeCounterPage> {
     'assets/dice5.svg',
     'assets/dice6.svg',
   ];
-  String currentDiceFace = 'assets/dice_face1.svg'; // Initial face
-  Color backgroundColor = Colors.white; // Default background color
+  String currentDiceFace = 'assets/dice1.svg';
+  Color backgroundColor = Colors.white;
   bool changeBackgroundColor = false;
+  bool isCounting = false;
 
   @override
   void initState() {
@@ -51,13 +59,17 @@ class _ShakeCounterPageState extends State<ShakeCounterPage> {
           event.z.abs() >= threshold) {
         setState(() {
           shakeCount++;
-          rollDice(); // Roll the dice on shake
-          Vibration.vibrate(duration: 10000);
+          Vibration.vibrate(duration: 100);
+          rollDice();
+          if (!isCounting) {
+            isCounting = true;
+            startCountdown();
+          }
 
           // Change background color and set a timer to reset it after 3 seconds
           changeBackgroundColor = true;
           backgroundColor = Colors.primaries[random.nextInt(Colors.primaries.length)];
-          Timer(Duration(seconds: 3), () {
+          Timer(const Duration(seconds: 3), () {
             setState(() {
               changeBackgroundColor = false;
               backgroundColor = Colors.white;
@@ -76,6 +88,24 @@ class _ShakeCounterPageState extends State<ShakeCounterPage> {
     });
   }
 
+  void startCountdown() {
+    Timer.periodic(const Duration(seconds: 1), (timer) {
+      setState(() {
+        countdown--;
+      });
+      if (countdown == 0) {
+        timer.cancel();
+        if (shakeCount == 11) {
+          score = 0;
+        } else {
+          score++;
+        }
+        isCounting = false;
+        countdown = 5;
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -83,7 +113,16 @@ class _ShakeCounterPageState extends State<ShakeCounterPage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            const Text(
+              'Number To Build',
+              style: TextStyle(fontSize: 24),
+            ),
             Text(
+              '11',
+              style: TextStyle(fontSize: 65),
+            ),
+            const SizedBox(height: 30),
+            const Text(
               'Shake Count:',
               style: TextStyle(fontSize: 24),
             ),
@@ -91,9 +130,17 @@ class _ShakeCounterPageState extends State<ShakeCounterPage> {
               '$shakeCount',
               style: TextStyle(fontSize: 48),
             ),
-            SizedBox(height: 20),
+            const SizedBox(height: 20),
+            Text(
+              'Score: $score',
+              style: TextStyle(fontSize: 36),
+            ),
+            Text(
+              'Countdown: $countdown seconds',
+              style: TextStyle(fontSize: 24),
+            ),
             GestureDetector(
-              onTap: rollDice, // Trigger the dice roll on tap
+              onTap: rollDice,
               child: SvgPicture.asset(
                 currentDiceFace,
                 width: 150,
